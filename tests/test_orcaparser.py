fd9e61f6ef0b057ebb8567ec1960b43a0bf918ea
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-10 18:56:00 +0200

    Adapted minor changes in repeating sections

--- tests/test_orcaparser.py
+++ tests/test_orcaparser.py
@@ -61,9 +61,9 @@ def test_scf(parser):
     assert len(sec_scc.scf_iteration) == 8
     assert sec_scc.scf_iteration[3].energy.total.value.magnitude == approx(-4.94113458e-16)
     assert sec_scc.scf_iteration[-1].x_orca_last_max_density_change == approx(9.441463170411847e-22)
-    assert np.shape(sec_scc.eigenvalues.value[0][0]) == (62,)
-    assert sec_scc.eigenvalues.value[0][0][28].magnitude == approx(6.53237991e-18)
-    assert sec_scc.eigenvalues.occupations[0][0][6] == 2.0
+    assert np.shape(sec_scc.eigenvalues[0].value[0][0]) == (62,)
+    assert sec_scc.eigenvalues[0].value[0][0][28].magnitude == approx(6.53237991e-18)
+    assert sec_scc.eigenvalues[0].occupations[0][0][6] == 2.0
     assert len(sec_scc.charges[0].value) == 2
     assert sec_scc.charges[0].value[0].magnitude == 0.131793
     assert sec_scc.charges[0].orbital_projected[27].value.magnitude == 0.027488
@@ -91,7 +91,7 @@ def test_spinpol(parser):
     parser.parse('tests/data/BO_spinpol/orca3.2984863.out', archive, None)
 
     assert archive.run[0].method[0].x_orca_multiplicity == 2
-    sec_eig = archive.run[0].calculation[0].eigenvalues
+    sec_eig = archive.run[0].calculation[0].eigenvalues[0]
     assert np.shape(sec_eig.value[1][0]) == (28,)
     assert sec_eig.value[1][0][22].magnitude == approx(7.57745431e-18)
     assert sec_eig.occupations[0][0][2] == 1.0
@@ -123,5 +123,5 @@ def test_tddft(parser):
 
     assert archive.run[0].method[1].electronic.method == 'TDDFT'
     sec_scc = archive.run[0].calculation[0]
-    assert sec_scc.excited_states.excitation_energies[8].magnitude == approx(7.85956552e-16)
-    assert sec_scc.excited_states.transition_dipole_moments[21][1].magnitude == approx(-2.96742377e-33)
+    assert sec_scc.excited_states[0].excitation_energies[8].magnitude == approx(7.85956552e-16)
+    assert sec_scc.excited_states[0].transition_dipole_moments[21][1].magnitude == approx(-2.96742377e-33)
